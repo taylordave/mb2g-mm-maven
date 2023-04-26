b9371170e7d740bce860ba84c1fceceeb21aa985
Dave Taylor <dave.taylor@functech.co.uk>
2023-04-26 13:25:42 +0100

    Add a dummy web-app to demonstrate creating dependencies between modules inside a project

---
+++ web-app/src/main/java/guru/springframework/controllers/UserController.java
@@ -0,0 +1,13 @@
+package guru.springframework.controllers;
+
+import guru.springframework.domain.UserCommand;
+import guru.springframework.entities.User;
+
+public class UserController {
+
+    User saveUser(UserCommand command) {
+
+        // fake impl
+        return new User();
+    }
+}
